c4d7c1f74fa297bb4bc2c31520437880ae45fd7c
Islom Nuridinov <islom.din.nur@gmail.com>
2021-09-19 17:19:14 +0500

    scrollable column

---
+++ app/src/main/java/com/example/composables/ScrollableColumn.kt
@@ -0,0 +1,39 @@
+package com.example.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ScrollableColumn() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .background(Color.Yellow)
+        ,
+    ) {
+        for(i in 1..50) {
+            Text(
+                text = "Item $i",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
+}
