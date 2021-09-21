092b0065bff4a82576de2bad9cb1021ead086412
Islom Nuridinov <islom.din.nur@gmail.com>
2021-09-21 22:21:32 +0500

    Lazy column - alternative for RecyclerView

---
+++ app/src/main/java/com/example/composables/LazyColumn.kt
@@ -0,0 +1,36 @@
+package com.example.composables
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LazyC() {
+    LazyColumn {
+        itemsIndexed(
+            listOf("This", "is", "Jetpack", "Compose")
+        ) { _, item ->
+            Text(
+                text = item,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+
+        items(5000) { itemIndex ->
+
+        }
+    }
+}
